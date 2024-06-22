ecf41fe46c617c36b7609a84e1b5126ab4dd9e2c
Swathika Durairaj <140158640+DSwathika@users.noreply.github.com>
2024-06-22 08:55:38 +0530

    Add files for library management system

---
+++ LoginPage.java
@@ -0,0 +1,168 @@
+package MyPackage;
+import javax.swing.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class LoginPage extends javax.swing.JFrame {
+
+    Connection con=null;
+
+    public LoginPage() {
+        initComponents();
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username= "scott";
+        String password = "scott";     
+        try{
+          Class.forName("oracle.jdbc.OracleDriver");
+          //JOptionPane.showMessageDialog(null,"Driver Loaded!");
+          con = DriverManager.getConnection(url,username,password);
+         }
+        
+        catch(ClassNotFoundException ex){
+           Logger.getLogger(LoginPage.class.getName()).log(Level.SEVERE,null, ex);
+           JOptionPane.showMessageDialog(null,"Could not find database driver!");
+         }
+        catch (SQLException ex) {
+           Logger.getLogger(LoginPage.class.getName()).log(Level.SEVERE,null, ex);
+           JOptionPane.showMessageDialog(null,"Failed to connect to the database");
+          }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        Admin = new javax.swing.JButton();
+        Student = new javax.swing.JButton();
+        Exit = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Perpetua Titling MT", 1, 14)); // NOI18N
+        jLabel1.setText("Library Management System");
+
+        Admin.setFont(new java.awt.Font("Lucida Fax", 0, 12)); // NOI18N
+        Admin.setText("Admin");
+        Admin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AdminActionPerformed(evt);
+            }
+        });
+
+        Student.setFont(new java.awt.Font("Lucida Bright", 0, 12)); // NOI18N
+        Student.setText("Student");
+        Student.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                StudentActionPerformed(evt);
+            }
+        });
+
+        Exit.setFont(new java.awt.Font("Lucida Bright", 0, 12)); // NOI18N
+        Exit.setText("Exit");
+        Exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ExitActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(78, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(Admin)
+                        .addGap(91, 91, 91)
+                        .addComponent(Student)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(75, 75, 75))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(153, 153, 153)
+                .addComponent(Exit, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addComponent(jLabel1)
+                .addGap(66, 66, 66)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Admin)
+                    .addComponent(Student))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                .addComponent(Exit)
+                .addGap(79, 79, 79))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void StudentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StudentActionPerformed
+        // TODO add your handling code here:
+        MyJFrame j=new MyJFrame();
+        j.setVisible(true);
+        this.setVisible(false);
+        
+    }//GEN-LAST:event_StudentActionPerformed
+
+    private void AdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AdminActionPerformed
+        // TODO add your handling code here:
+        adminLogin a=new adminLogin();
+        a.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_AdminActionPerformed
+
+    private void ExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExitActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_ExitActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginPage().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Admin;
+    private javax.swing.JButton Exit;
+    private javax.swing.JButton Student;
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
